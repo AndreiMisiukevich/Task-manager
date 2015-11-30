95bd13e554c847292d65a30aa636d2ab8a2a8423
Andrei Misukevich <andrei.misiukevich@gambino.by>
2015-11-30 11:40:39 +0300

    Added taskObserver feature.

---
+++ ManagingProcesses/ManagingProcesses/MostFatProcesses.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ManagingProcesses
+{
+    partial class MostFatProcesses
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(23, 17);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 98);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(23, 121);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(258, 98);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Info;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(23, 225);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(258, 98);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(288, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 97);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(288, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 97);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(288, 225);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 97);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // MostFatProcesses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 334);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "MostFatProcesses";
+            this.Text = "MostFatProcesses";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ ManagingProcesses/ManagingProcesses/MostFatProcesses.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace ManagingProcesses
+{
+    public partial class MostFatProcesses : Form
+    {
+        private const string Pattern = "ID: {0};\r\n Name : {1};\r\n Memory {2} (Mb);";
+        private CustomProcessInfo[] _procArr;
+
+        public MostFatProcesses()
+        {
+
+            InitializeComponent();
+        }
+
+        public void SetNewProcesses(IEnumerable<CustomProcessInfo> processes)
+        {
+            _procArr = processes.ToArray();
+            Array.Sort(_procArr, (a, b) => b.Memory > a.Memory ? 1 : b.Memory == a.Memory ? 0 : -1);
+
+            textBox1.AppendText(string.Format(Pattern, _procArr[0].Id, _procArr[0].Name, _procArr[0].Memory));
+            textBox2.AppendText(string.Format(Pattern, _procArr[1].Id, _procArr[1].Name, _procArr[1].Memory));
+            textBox3.AppendText(string.Format(Pattern, _procArr[2].Id, _procArr[2].Name, _procArr[2].Memory));
+       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StopProc(_procArr[0].Id);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StopProc(_procArr[1].Id);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StopProc(_procArr[2].Id);
+        }
+
+        private void StopProc(int id)
+        {
+            try
+            {
+                var processes = Process.GetProcesses();
+                var curProc = processes.FirstOrDefault(x => x.Id == id);
+                if (curProc != null)
+                {
+                    curProc.Kill();
+                }
+            }
+            catch
+            {
+                MessageBox.Show(@"System process! Canceled", @"Error");
+            }
+        }
+
+
+        private void Clear()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+    }
+
+    public class CustomProcessInfo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public BigInteger Memory { get; set; }
+
+    }
+}
